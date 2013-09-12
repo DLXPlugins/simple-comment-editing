7b896561f8592e6c9e08d5a77891dbbad0fd2cbc
Ronald Huereca <ronalfy@gmail.com>
2013-09-12 15:38:10 +0200

    Updating deploy script.
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,69 @@
 #! /bin/bash
-# A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
-# The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
+# See https://github.com/GaryJones/wordpress-plugin-git-flow-svn-deploy for instructions and credits.
 
-# main config
-PLUGINSLUG="simple-comment-editing"
+echo
+echo "WordPress Plugin Git-Flow SVN Deploy v1.0.0-dev"
+echo
+echo "Step 1. Let's collect some information first."
+echo
+echo "Default values are in brackets - just hit enter to accept them."
+echo
+
+# Get some user input
+# Can't use the -i flag for read, since that doesn't work for bash 3
+read -e -p "1a) WordPress Repo Plugin Slug e.g. my-awesome-plugin: " PLUGINSLUG
+echo
+
+# Set up some default values. Feel free to change these in your own script
 CURRENTDIR=`pwd`
-MAINFILE="index.php" # this should be the name of your main php file in the wordpress plugin
+default_svnpath="/tmp/$PLUGINSLUG"
+default_svnurl="http://plugins.svn.wordpress.org/$PLUGINSLUG"
+
+echo -e "Enter your wp.org username: \c"
+read default_svnuser
+default_plugindir="$CURRENTDIR/$PLUGINSLUG"
+default_mainfile="index.php"
+
+echo "1b) Path to a local directory where a temporary SVN checkout can be made."
+read -e -p "No trailing slash and don't add trunk ($default_svnpath): " input
+SVNPATH="${input:-$default_svnpath}"
+echo
+
+echo "1c) Remote SVN repo on WordPress.org. No trailing slash."
+read -e -p "($default_svnurl): " input
+SVNURL="${input:-$default_svnurl}"
+echo
+
+read -e -p "1d) Your WordPress repo SVN username ($default_svnuser): " input
+SVNUSER="${input:-$default_svnuser}"
+echo
+
+echo "1e) Your local plugin root directory, the Git repo."
+read -e -p "($default_plugindir): " input
+PLUGINDIR="${input:-$default_plugindir}"
+echo
+
+read -e -p "1f) Name of the main plugin file ($default_mainfile): " input
+MAINFILE="${input:-$default_mainfile}"
+echo
+
+echo "That's all of the data collected."
+echo
+echo "Slug: $PLUGINSLUG"
+echo "Temp checkout path: $SVNPATH"
+echo "Remote SVN repo: $SVNURL"
+echo "SVN username: $SVNUSER"
+echo "Plugin directory: $PLUGINDIR"
+echo "Main file: $MAINFILE"
+echo
 
 # git config
-GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
-
-# svn config
-SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/simple-comment-editing/" # Remote SVN repo on wordpress.org, with no trailing slash
-echo -e "Enter your wp.org username: \c"
-read SVNUSER
+GITPATH="$PLUGINDIR/" # this file should be in the base of your git repository
 
 # Let's begin...
 echo ".........................................."
 echo 
-echo "Preparing to deploy wordpress plugin"
+echo "Preparing to deploy WordPress plugin"
 echo 
 echo ".........................................."
 echo 
@@ -27,59 +71,93 @@ echo
 # Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
 NEWVERSION1=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}'`
 echo "readme.txt version: $NEWVERSION1"
-NEWVERSION2=`grep "^Version:" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}'`
+NEWVERSION2=`grep "Version:" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}'`
 echo "$MAINFILE version: $NEWVERSION2"
 
 if [ "$NEWVERSION1" != "$NEWVERSION2" ]; then echo "Version in readme.txt & $MAINFILE don't match. Exiting...."; exit 1; fi
 
 echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
 
-if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
-	then 
-		echo "Version $NEWVERSION1 already exists as git tag. Exiting...."; 
-		exit 1; 
-	else
-		echo "Git version does not exist. Let's proceed..."
-fi
+# GaryJ: Ignore check for git tag, as git flow release finish creates this.
+#if git show-ref --tags --quiet --verify -- "refs/tags/$NEWVERSION1"
+#	then 
+#		echo "Version $NEWVERSION1 already exists as git tag. Exiting...."; 
+#		exit 1; 
+#	else
+#		echo "Git version does not exist. Let's proceed..."
+#fi
 
+echo "Changing to $GITPATH"
 cd $GITPATH
-echo -e "Enter a commit message for this new version: \c"
-read COMMITMSG
-git commit -am "$COMMITMSG"
+# GaryJ: Commit message variable not needed . Hard coded for SVN trunk commit for consistency.
+#echo -e "Enter a commit message for this new version: \c"
+#read COMMITMSG
+# GaryJ: git flow release finish already covers this commit.
+#git commit -am "$COMMITMSG"
 
-echo "Tagging new version in git"
-git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
+# GaryJ: git flow release finish already covers this tag creation.
+#echo "Tagging new version in git"
+#git tag -a "$NEWVERSION1" -m "Tagging version $NEWVERSION1"
 
-echo "Pushing latest commit to origin, with tags"
+echo "Pushing git master to origin, with tags"
 git push origin master
 git push origin master --tags
 
 echo 
-echo "Creating local copy of SVN repo ..."
-svn co $SVNURL $SVNPATH
+echo "Creating local copy of SVN repo trunk ..."
+svn checkout $SVNURL/trunk $SVNPATH/trunk
+
+echo "Ignoring GitHub specific files"
+svn propset svn:ignore "README.md
+Thumbs.db
+.git
+.gitignore" "$SVNPATH/trunk/"
 
 echo "Exporting the HEAD of master from git to the trunk of SVN"
 git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
-echo "Ignoring github specific files and deployment script"
-svn propset svn:ignore "deploy.sh
-README.md
-.git
-.gitignore" "$SVNPATH/trunk/"
+# If submodule exist, recursively check out their indexes
+if [ -f ".gitmodules" ]
+	then
+		echo "Exporting the HEAD of each submodule from git to the trunk of SVN"
+		git submodule init
+		git submodule update
+		git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'
+fi
+
+# Support for the /assets folder on the .org repo.
+echo "Moving assets"
+# Make the directory if it doesn't already exist
+mkdir $SVNPATH/assets/
+mv $SVNPATH/trunk/assets/* $SVNPATH/assets/
+svn add $SVNPATH/assets/
+svn delete $SVNPATH/trunk/assets
 
 echo "Changing directory to SVN and committing to trunk"
 cd $SVNPATH/trunk/
+# Delete all files that should not now be added.
+svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2}' | xargs svn del
 # Add all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
-svn commit --username=$SVNUSER -m "$COMMITMSG"
+svn commit --username=$SVNUSER -m "Preparing for $NEWVERSION1 release"
 
-echo "Creating new SVN tag & committing it"
+echo "Updating WordPress plugin repo assets and committing"
+cd $SVNPATH/assets/
+# Add all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2}' | xargs svn del
+# Add all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+svn commit --username=$SVNUSER -m "Updating assets"
+
+echo "Creating new SVN tag and committing it"
 cd $SVNPATH
 svn copy trunk/ tags/$NEWVERSION1/
 cd $SVNPATH/tags/$NEWVERSION1
 svn commit --username=$SVNUSER -m "Tagging version $NEWVERSION1"
 
 echo "Removing temporary directory $SVNPATH"
+cd $SVNPATH
+cd ..
 rm -fr $SVNPATH/
 
 echo "*** FIN ***"
