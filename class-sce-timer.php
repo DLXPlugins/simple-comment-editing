aa1e3f67ad64dc3c79f0472fb62570fc8ba7e2b4
Ronald Huereca <ronalfy@gmail.com>
2015-06-15 10:31:31 -0500

    Adding sce timer class.
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

---
+++ class-sce-timer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Simple Comment Editing controller for internationalized timers
+ *
+ * Generates internationalized timer variables for use in JavaScript
+ *
+ * @since 1.3.0
+ *
+ * @package WordPress
+ */
+ class SCE_Timer {
+	public function __construct() {
+		
+	}
+	
+	public function get_timer_vars() {
+		
+	}	 
+}
