d2b8efc467609e8cd3fe81443f8e4338385558b0
Ronald Huereca <ronalfy@gmail.com>
2015-06-15 12:41:53 -0500

    Adding delete transient locale to uninstall
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

--- uninstall.php
+++ uninstall.php
@@ -7,4 +7,5 @@ if ( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') ) {
 	$sql = "delete from $wpdb->postmeta where left(meta_value, 6) = 'wpAjax'";
 	$wpdb->query($sql);
 	delete_option( 'ajax-edit-comments_security_key_count' );
+	delete_transient( 'sce_timer_' . get_locale() );
 ?>
