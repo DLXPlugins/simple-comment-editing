1aa0283c76830d75c0d7beb7bbb8765ad1384e55
Ronald Huereca <ronalfy@gmail.com>
2015-06-15 12:40:13 -0500

    Adding inline docs.
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

--- class-sce-timer.php
+++ class-sce-timer.php
@@ -13,6 +13,16 @@
 		
 	}
 	
+	/**
+	* Return internationalized array of seconds/minutes variables
+	*
+	* Return internationalized array of seconds/minutes variables
+	*
+	* @since 1.3.0
+	* @access public
+	*
+	* @return associative array of seconds/minutes internationalized variables
+	*/
 	public function get_timer_vars( $force_transient = false ) {
 		$locale = get_locale();
 		$timer_vars_transient = get_transient( 'sce_timer_' . $locale );
@@ -28,9 +38,31 @@
 		}
 		return array();
 	}
+	
+	/**
+	* Return the comment edit time in minutes
+	*
+	* Return the comment edit time in minutes
+	*
+	* @since 1.3.0
+	* @access private
+	*
+	* @return int comment time
+	*/
 	private function get_comment_time() {
 		return absint( Simple_Comment_Editing::get_instance()->get_comment_time() );
 	}	 
+	
+	/**
+	* Return array of internationalized minutes
+	*
+	* Return array of internationalized minutes
+	*
+	* @since 1.3.0
+	* @access private
+	*
+	* @return array minutes
+	*/
 	private function get_minutes() {
 		$comment_minutes = $this->get_comment_time();
 		$minutes = array();
@@ -39,6 +71,17 @@
 		}
 		return $minutes;
 	}
+	
+	/**
+	* Return array of internationalized seconds
+	*
+	* Return array of internationalized seconds
+	*
+	* @since 1.3.0
+	* @access private
+	*
+	* @return array seconds
+	*/
 	private function get_seconds() {
 		$seconds = array();
 		for( $i = 0; $i < 60; $i++ ) {
