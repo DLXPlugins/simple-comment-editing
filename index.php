56d0631ffe37a5d50c399075e951b46e1d357ff2
Ronald Huereca <ronalfy@gmail.com>
2015-06-18 10:18:19 -0500

    Updating version.
    Signed-off-by: Ronald Huereca <ronalfy@gmail.com>

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: Simple Comment Editing
 Plugin URI: http://wordpress.org/extend/plugins/simple-comment-editing/
 Description: Simple comment editing for your users.
 Author: ronalfy
-Version: 1.2.4
+Version: 1.3.0
 Requires at least: 3.5
 Author URI: http://www.ronalfy.com
 Contributors: ronalfy
