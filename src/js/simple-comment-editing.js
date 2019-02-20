2e7882bb76c7067202b4ca50a534b7aaeeddf16b
Ronald Huereca <ronald@mediaron.com>
2019-02-20 14:58:12 -0600

    Returning if comment is unlimited.

--- src/js/simple-comment-editing.js
+++ src/js/simple-comment-editing.js
@@ -169,6 +169,9 @@ jQuery( document ).ready( function( $ ) {
 			*/
 			$.post( ajax_url, { action: 'sce_get_time_left', comment_id: ajax_params.cid, post_id: ajax_params.pid, _ajax_nonce: simple_comment_editing.nonce }, function( response ) {
 				//Set initial timer text
+				if( 'unlimited' === minutes && 'unlimited' === seconds ) {
+					return;
+				}
 				var minutes = parseInt( response.minutes );
 				var seconds = parseInt( response.seconds );
 				var timer_text = sce.get_timer_text( minutes, seconds );
