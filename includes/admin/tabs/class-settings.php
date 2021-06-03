817baa167cd43a6a9c872c729fbfcfa3040523e3
Ronald Huereca <ronalfy@gmail.com>
2021-06-03 08:13:41 -0500

    Adding SCE dark/light/default popups.

--- includes/admin/tabs/class-settings.php
+++ includes/admin/tabs/class-settings.php
@@ -114,22 +114,28 @@ class Settings extends Tabs {
 										<option value="dark" <?php selected( 'dark', $options['button_theme'] ); ?> ><?php esc_html_e( 'Dark', 'simple-comment-editing-options' ); ?></option>
 										<option value="light" <?php selected( 'light', $options['button_theme'] ); ?>><?php esc_html_e( 'Light', 'simple-comment-editing-options' ); ?></option>
 									</select>
+									<input type="hidden" value="false" name="options[show_icons]" />
+									<p><input id="sce-allow-icons" type="checkbox" value="true" name="options[show_icons]" <?php checked( true, $options['show_icons'] ); ?> /> <label for="sce-allow-icons"><?php esc_html_e( 'Allow icons for the buttons. Recommended if you have selected a button theme.', 'simple-comment-editing-options' ); ?></label></p>
 									<p class="sce-theme-preview">
 										<strong>
 										<?php
 											esc_html_e( 'Button Theme Preview:', 'simple-comment-editing' );
 										?>
 										</strong>
-										<a data-animation-effect="zoom" data-animation-duration="1000" data-fancybox data-src="#sce-screenshot-default" data-caption="SCE Default Theme" href="javascript:;">Regular</a> | Dark | Light
+										<a data-animation-effect="zoom" data-animation-duration="1000" data-fancybox data-src="#sce-screenshot-default" data-caption="SCE Default Theme" href="javascript:;"><?php esc_html_e( 'Default Theme', 'simple-comment-editing' ); ?></a> | <a data-animation-effect="zoom" data-animation-duration="1000" data-fancybox data-src="#sce-screenshot-dark" data-caption="SCE Dark Theme" href="javascript:;"><?php esc_html_e( 'Dark Theme', 'simple-comment-editing' ); ?></a> | <a data-animation-effect="zoom" data-animation-duration="1000" data-fancybox data-src="#sce-screenshot-light" data-caption="SCE Light Theme" href="javascript:;"><?php esc_html_e( 'Light Theme', 'simple-comment-editing' ); ?></a>
 									</p>
-									<input type="hidden" value="false" name="options[show_icons]" />
-									<p><input id="sce-allow-icons" type="checkbox" value="true" name="options[show_icons]" <?php checked( true, $options['show_icons'] ); ?> /> <label for="sce-allow-icons"><?php esc_html_e( 'Allow icons for the buttons. Recommended if you have selected a button theme.', 'simple-comment-editing-options' ); ?></label></p>
 								</td>
 							</tr>
 								</tbody>
 							</table>
-							<div id="sce-screenshot-default" style="display: none; width: 100%; max-width: 600px">
+							<div id="sce-screenshot-default" style="display: none;">
 								<img src="<?php echo esc_url( Functions::get_plugin_url( '/images/screenshot-theme-default.png' ) ); ?>" alt="SCE Default Theme Screenshot" />
+							</div>
+							<div id="sce-screenshot-dark" style="display: none;">
+								<img src="<?php echo esc_url( Functions::get_plugin_url( '/images/screenshot-theme-dark.png' ) ); ?>" alt="SCE Dark Theme Screenshot" />
+							</div>
+							<div id="sce-screenshot-light" style="display: none;">
+								<img src="<?php echo esc_url( Functions::get_plugin_url( '/images/screenshot-theme-light.png' ) ); ?>" alt="SCE Light Theme Screenshot" />
 							</div>
 							
 							<?php submit_button( __( 'Save Options', 'simple-comment-editing' ), 'sce-button sce-button-info', 'submit', true ); ?>
